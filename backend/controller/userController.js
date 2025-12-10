eabaa74f6d1b17044e143a76fa519600f017e7c2
Amirul Ikhwan <amirikhwanfaisal@gmail.com>
2025-12-11 00:57:49 +0800

    update and create task

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -9,15 +9,22 @@ export const get_dashboard = catchAsync(async (req , res) => {
     const [rows] = await db.query(
         `
         SELECT 
-            u.user_name AS userName , 
-            g.group_name AS groupName , 
+            u.user_name AS userName,
+            g.group_name AS groupName,
             COUNT(t.title) AS totalTask,
-            ROUND((SUM(t.status = 'Finish')/COUNT(t.title))*100 , 2) AS totalComplete
-        FROM USERS u  
-        JOIN  TASK t ON u.user_id = t.user_id
-        JOIN GROUP_TASK g ON t.group_id = g.group_id
+            COALESCE(
+                ROUND(
+                    (SUM(CASE WHEN t.status = 'Finish' THEN 1 ELSE 0 END) /
+                    NULLIF(COUNT(t.title), 0)) * 100,
+                2),
+            0) AS totalComplete
+        FROM USERS u
+        JOIN MEMBERS m ON u.user_id = m.user_id    
+        JOIN GROUP_TASK g ON m.group_id = g.group_id    
+        LEFT JOIN TASK t ON g.group_id = t.group_id     
         WHERE u.user_id = ?
-        GROUP BY u.user_name , g.group_name;
+        GROUP BY u.user_name, g.group_name;
+
         `,
         [user.user_id]
     )
@@ -63,16 +70,17 @@ export const add_task = catchAsync( async (req , res) => {
 
     const user = req.user;
     const groupId = req.params.groupId;
+    const db = req.app.locals.db;
 
     const {picId , title , desc , due} = req.body;
 
-
+    const status = "Prep"
     const [result] = await db.query(
-            `INSERT INTO 
-                ( group_id , created_by , userId , title , task_desc , status , created_at , due_date)
+            `INSERT INTO TASK 
+                ( group_id , created_by , user_id , title , task_desc , status , created_at , due_date)
             VALUES( ? , ? , ? , ? , ? , ?  , ? , ?)
             `,
-            [groupId , user.user_id , picId ,title , desc , "Prep" , new Date() , due ]
+            [groupId , user.user_id , picId ,title , desc , status , new Date() , due ]
     );
 
     if(result.affectedRows === 0){
@@ -105,7 +113,7 @@ export const create_group = catchAsync( async (req , res) => {
     const groupId = result.insertId;
 
     const [resultInsert] = await db.query(
-        'INSERT INTO MEMBER_OF (user_id , group_id , joined_at) VALUES (? , ? , ?)',
+        'INSERT INTO MEMBERS (user_id , group_id , joined_at) VALUES (? , ? , ?)',
         [user.user_id , groupId , new Date()]
     )
 
@@ -116,4 +124,95 @@ export const create_group = catchAsync( async (req , res) => {
     res.status(201).json({success : true , msg : `Successfully create ${groupName}`})
 
     
+})
+
+export const get_notes = catchAsync( async(req , res) => {
+    
+    const user = req.user;
+    const db = req.app.locals.db;
+
+    const [rows] = await db.query(
+        `
+        SELECT 
+            g.group_name AS groupName , 
+            n.note_types  AS types, 
+            n.note_details AS details 
+        FROM NOTES n JOIN GROUP_TASK g ON g.group_id = n.group_id
+        WHERE g.group_id  IN ( SELECT group_id FROM members WHERE user_id = ?);
+        `,
+        [user.user_id]
+    )
+
+    const result = rows.reduce((acc, item) => {
+        if (!acc[item.groupName]) acc[item.groupName] = [];
+        acc[item.groupName].push(item);
+        return acc;
+    }, {});
+
+    res.status(200).json({success : true , notes : result})
+
+} )
+
+export const add_notes = catchAsync( async(req , res) => {
+
+    const groupId = req.params.groupId;
+    const db = req.app.locals.db;
+    const {noteTypes , noteDetails} = req.body;
+
+    const [result] = await db.query(
+        `
+        INSERT INTO NOTES (group_id , note_types , note_details)
+        VALUES(? , ? , ?)
+        `,
+        [groupId , noteTypes , noteDetails]
+    );
+
+    if(result.affectedRows === 0){
+        throw new AppError("Fail Add Data" , 400)
+    }
+
+    const noteId = result.insertId;
+
+    const [rows] = await db.query(
+        `
+        SELECT 
+            g.group_name AS groupName , 
+            n.note_types AS types , 
+            n.note_details AS details 
+        FROM NOTES n JOIN GROUP_TASK g ON g.group_id = n.group_id
+        WHERE note_id = ?;
+        `,
+        [noteId]
+    )
+
+    if(rows.length === 0){
+        throw new AppError('notes not found' , 404)
+    }
+
+    res.status(201).json({success : true , message : "Successfully Add Notes" , data : rows[0]})
+
+})
+
+export const update_task = catchAsync( async(req , res) => {
+
+
+    const db = req.app.locals.db;
+    const taskId = req.params.taskId;
+
+    const {status} = req.body;
+
+    const [result] = await db.query(
+        `
+        UPDATE TASK
+        SET STATUS = ?
+        WHERE TASK_ID = ?
+        `,
+        [status , taskId]
+    )
+
+    if(result.affectedRows === 0){
+        throw new AppError('Fail Updated the status' , 400)
+    }
+
+    res.status(201).json({success : true , message : 'successfully change the status'})
 })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { 
+    add_notes,
     add_task,
     create_group, 
     get_dashboard, 
-    get_task 
+    get_notes, 
+    get_task, 
+    update_task
 } from "../controller/userController.js";
 
 
@@ -11,8 +14,12 @@ const router = Router();
 
 router.get('/tasks' , get_task);
 router.post('/tasks/:groupId' , add_task);
+router.put('/task/:taskId' , update_task);
 
 router.get('/groups' , get_dashboard);
 router.post('/groups' , create_group);
+
+router.get('/notes' , get_notes);
+router.post('/notes/:groupId' , add_notes );
 
 export default router;
